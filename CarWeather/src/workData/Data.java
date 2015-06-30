f3d04964b8f0285271eb19c6082c4b0581be8bf3
Luca Tomaselli <luc.tomaselli@stud.uniroma3.it>
2015-06-30 11:59:30 +0200

    aggiunto metodo per le fascie orarie

--- CarWeather/src/workData/Data.java
+++ CarWeather/src/workData/Data.java
@@ -1,6 +1,9 @@
 package workData;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class Data {
 	
@@ -29,8 +32,35 @@ public class Data {
 	    return false;
 	}
 	
+	public static int fasciaOraria(String data){
+		SimpleDateFormat sdf = new SimpleDateFormat("HHmmss");
+		try {
+			Date zero = sdf.parse("000000");
+			Date sei = sdf.parse("060000");
+			Date mezzogiorno = sdf.parse("120000");
+			Date diciotto = sdf.parse("180000");
+			Date mezzanotte = sdf.parse("240000");
+			Date d = sdf.parse(data.substring(8));
+			//System.out.println(d);
+			if (d.equals(zero) || (d.after(zero) && d.before(sei)))
+				return 0;
+			if (d.equals(sei) || (d.after(sei) && d.before(mezzogiorno)))
+				return 1;
+			if (d.equals(mezzogiorno) || (d.after(mezzogiorno) && d.before(diciotto)))
+				return 2;
+			if (d.equals(diciotto) || (d.after(diciotto) && d.before(mezzanotte)))
+				return 3;
+			else
+				return -1;
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(Data.isFestivity("20150501"));
+		System.out.println(Data.isFestivity("20150314125559"));
+		System.out.println(Data.fasciaOraria("20150314125559"));
 	}
 
 }
